6f537e17381dfca6a4d15bd10901febdcaafe9bc
JulioMaiaB <julio_maia1@hotmail.com>
2021-03-04 18:33:57 -0300

    H2 database configuration

--- src/main/java/com/juliobeani/cursomc/domain/Category.java
+++ src/main/java/com/juliobeani/cursomc/domain/Category.java
@@ -7,9 +7,6 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
-import lombok.EqualsAndHashCode;
-
-@EqualsAndHashCode
 @Entity(name = "tb_category")
 public class Category implements Serializable {
 	private static final long serialVersionUID = 1L;
@@ -18,9 +15,10 @@ public class Category implements Serializable {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String name;
-	
-	public Category () {}
-	
+
+	public Category() {
+	}
+
 	public Category(Integer id, String name) {
 		this.id = id;
 		this.name = name;
@@ -40,5 +38,30 @@ public class Category implements Serializable {
 
 	public void setName(String name) {
 		this.name = name;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Category other = (Category) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
 	}
 }
