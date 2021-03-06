e7bdd93b5ac649f07a68f73a3dabb59ddbc96c3f
JulioMaiaB <julio_maia1@hotmail.com>
2021-03-06 18:13:49 -0300

    Creating instatiation operation

--- src/main/java/com/juliobeani/cursomc/CursomcApplication.java
+++ src/main/java/com/juliobeani/cursomc/CursomcApplication.java
@@ -1,13 +1,30 @@
 package com.juliobeani.cursomc;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.juliobeani.cursomc.domain.Category;
+import com.juliobeani.cursomc.repositories.CategoryRepository;
+
 @SpringBootApplication
-public class CursomcApplication {
+public class CursomcApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoryRepository CategoryRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		Category cat1 = new Category(null, "Informática");
+		Category cat2 = new Category(null, "Escritório");
+		// Operacao para salvar as categorias no banco
+		CategoryRepository.saveAll(Arrays.asList(cat1, cat2));
+	}
 }
