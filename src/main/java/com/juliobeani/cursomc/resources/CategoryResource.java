860e2bd71219ab8c0f746a2f9568e3449297ae50
JulioMaiaB <julio_maia1@hotmail.com>
2021-03-11 03:14:28 -0300

    Inserting a new Category with POST

--- src/main/java/com/juliobeani/cursomc/resources/CategoryResource.java
+++ src/main/java/com/juliobeani/cursomc/resources/CategoryResource.java
@@ -1,11 +1,16 @@
 package com.juliobeani.cursomc.resources;
 
+import java.net.URI;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.juliobeani.cursomc.domain.Category;
 import com.juliobeani.cursomc.services.CategoryService;
@@ -22,5 +27,13 @@ public class CategoryResource {
 	public ResponseEntity<Category> findById (@PathVariable Integer id){
 		Category obj = service.findById(id);
 		return ResponseEntity.ok().body(obj);
+	}
+	
+	@PostMapping
+	public ResponseEntity<Void> insert(@RequestBody Category obj) {
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+				.path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
 	}
 }
