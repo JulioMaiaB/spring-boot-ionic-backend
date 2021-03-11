314726bdbc6847c4d5ad9f27b6a5d1fdb1d9fefc
JulioMaiaB <julio_maia1@hotmail.com>
2021-03-11 18:00:24 -0300

    Pagination with optional parameters in the request

--- src/main/java/com/juliobeani/cursomc/services/CategoryService.java
+++ src/main/java/com/juliobeani/cursomc/services/CategoryService.java
@@ -5,6 +5,9 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.juliobeani.cursomc.domain.Category;
@@ -14,25 +17,25 @@ import com.juliobeani.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoryService {
-	
+
 	@Autowired
 	private CategoryRepository repository;
-	
+
 	public List<Category> findAll() {
 		return repository.findAll();
 	}
-	
+
 	public Category findById(Integer id) {
-		Optional <Category> obj = repository.findById(id);
+		Optional<Category> obj = repository.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				"Objeto não encontrado! Id: " + id + ", Tipo: " + Category.class.getName()));
 	}
-	
+
 	public Category insert(Category obj) {
 		obj.setId(null); // Garante que estou inserindo um objeto novo
 		return repository.save(obj);
 	}
-	
+
 	public Category update(Category obj, Integer id) {
 		findById(id); // Apenas para verificar se o id do objeto nao e nulo
 		return repository.save(obj); // Como o id nao e nulo, e feita uma atualizacao de forma automatica
@@ -42,9 +45,14 @@ public class CategoryService {
 		findById(id);
 		try {
 			repository.deleteById(id);
-		}
-		catch(DataIntegrityViolationException e) {
+		} catch (DataIntegrityViolationException e) {
 			throw new DataIntegrityException("It is not possible to delete a category with products!");
 		}
+	}
+
+	public Page<Category> findPage(Integer page, Integer linesPerPage, String orderBy, 
+			String direction) {
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		return repository.findAll(pageRequest);
 	}
 }
